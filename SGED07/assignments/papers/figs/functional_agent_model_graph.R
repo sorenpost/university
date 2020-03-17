57db6111a304a233c0c6f221272a52362a2761e6
Soren Post <utv13spo@student.lu.se>
2020-03-17 03:56:56 +0100

    Much better

--- SGED07/assignments/papers/figs/functional_agent_model_graph.R
+++ SGED07/assignments/papers/figs/functional_agent_model_graph.R
@@ -1,5 +1,6 @@
-
+library(ggsci)
 library(tidyverse)
+library(ggpubr)
 library(ggraph)
 library(igraph)
 library(wesanderson)
@@ -20,3 +21,30 @@ ggraph(model_gr, layout = 'linear') +
   scale_color_manual(values = wes_palette("Darjeeling2")) +
   theme(legend.position='none')
 
+model_gr1 <- graph_from_literal(1 -+ 2, 2 -+ 3, 3 -+ 4) 
+
+g1 <- ggraph(model_gr1) +
+  geom_edge_link(arrow = arrow(length = unit(3, 'mm')),
+                start_cap = circle(5, 'mm'),
+                end_cap = circle(5, 'mm'),
+                strength = 0.5, # bend of curve
+                fold = FALSE) + 
+  geom_node_point(size = 10, color = "#0073C299") +
+  geom_node_text(aes(label = name)) +
+  theme_graph() +
+  theme(legend.position='none')
+
+model_gr2 <- graph_from_literal(1 -+ 2, 1 -+ 3, 1 -+ 4) 
+
+g2 <- ggraph(model_gr2) +
+  geom_edge_link(arrow = arrow(length = unit(3, 'mm')),
+                start_cap = circle(7, 'mm'),
+                end_cap = circle(7, 'mm'),
+                strength = 0.5, # bend of curve
+                fold = FALSE) + 
+  geom_node_point(size = 10, color = "#0073C299") +
+  geom_node_text(aes(label = name)) +
+  theme_graph() +
+  theme(legend.position='none')
+
+ggarrange(g1, g2, nrow = 1, ncol = 2, labels = c("A", "B"))
